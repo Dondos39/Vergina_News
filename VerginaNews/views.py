be00bc2f8b38e7d23bf2db2687c595bf47f62bf5
pagdas95 <pantelisa1995@gmail.com>
2022-07-05 13:15:14 -0700

    ora na ta klisw

--- VerginaNews/views.py
+++ VerginaNews/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 import categories.models
 import articles.models
+import authors.models
+
 
 class HomepageViews(ListView):
     #context_object_name = 'categories'
@@ -13,9 +15,11 @@ class HomepageViews(ListView):
         context = super(HomepageViews, self).get_context_data(**kwargs)
         context.update({
             'categories_list': categories.models.Category.objects.all(),
+            'authors_list': authors.models.Author.objects.all(),
             'important_list': articles.models.Article.objects.get_important().values('id', 'no_important', 'title'),
             'roaming_news_list': articles.models.Article.objects.get_latest(),
-            'frontnews_list': articles.models.Article.objects.get_frontnews().order_by('no_homepage'),
+            'frontnews_list': articles.models.Article.objects.get_frontnews().extra(select={'no_homepage': 'CAST(no_homepage AS INTEGER)'}).order_by('no_homepage'),
 
         })
+        print(context['frontnews_list'])
         return context
