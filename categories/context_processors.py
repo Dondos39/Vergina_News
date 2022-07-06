6bc808a6128d65f151a923bfcc4131f59b9b23da
pagdas95 <pantelisa1995@gmail.com>
2022-07-06 10:24:59 -0700

    my last changes

--- categories/context_processors.py
+++ categories/context_processors.py
@@ -3,3 +3,6 @@ from  .models import Category
 def menu_links(request):
     links = Category.objects.all()
     return dict(links=links)
+
+
+    
--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Category
+from .models import Category , SubCategory
 
 # Create your views here.
 class CategoryView(DetailView):
@@ -12,6 +12,20 @@ class CategoryView(DetailView):
         category = self.kwargs.get('category')
 
         detail = Category.objects.all().filter(name=category)
+        context = {
+                "detail": detail,
+        }
+        return render(request, "category.html", context=context)
+
+class SubCategoryView(DetailView):
+    context_object_name = 'sub_category'
+    template_name = 'categories.html'
+    model = SubCategory
+
+    def get(self, request, *args, **kwargs):
+        sub_category = self.kwargs.get('sub_category')
+
+        detail = SubCategory.objects.all().filter(name=sub_category)
         context = {
                 "detail": detail,
         }
