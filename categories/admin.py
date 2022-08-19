9a8e03480e21c51418c65efc452da6e7ce0106e9
Theodoros <Dondos39@gmail.com>
2022-08-19 20:54:08 +0300

    Added filters to subcategroy admin

--- categories/admin.py
+++ categories/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    readonly_fields = ['slug']
+
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
+    list_display = ('name', 'category', 'description')
+
+    readonly_fields = ['slug']
+    list_filter = ('category__name',)
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
