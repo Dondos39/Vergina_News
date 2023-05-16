9f499e4644b18f6e5e8683390e10de240a29fe80
Theodoros <Dondos39@gmail.com>
2023-05-16 13:03:58 +0300

    Added img-fluid to images

--- static/ckeditor/ckeditor/config.js
+++ static/ckeditor/ckeditor/config.js
@@ -3,8 +3,21 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+ CKEDITOR.on('instanceReady', function (ev) {
+    ev.editor.dataProcessor.htmlFilter.addRules(
+     {
+        elements:
+         {
+           $: function (element) {
+             // check for the tag name
+             if (element.name == 'img') {
+
+                 element.attributes.class = "img-fluid" // Put your class name here
+             }
+
+             // return element with class attribute
+             return element;
+          }
+        }
+    });
+  });
